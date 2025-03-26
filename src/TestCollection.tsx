f89a0eba543679cca2df2846bd37e72234ee1a5b
Alice Sebego <alice.etvotrecom@gmail.com>
2025-03-26 17:05:32 +0100

    feat: create a login form

--- src/TestCollection.tsx
+++ src/TestCollection.tsx
@@ -1,6 +1,7 @@
 // TestCollection.tsx
 import React from "react";
 import { db, collection, addDoc } from "./firebaseConfig";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 // Définition du type pour les données
 interface TestData {
@@ -10,26 +11,51 @@ interface TestData {
 }
 
 const TestCollection: React.FC = () => {
-  const addTestData = async () => {
-    try {
-      const newDoc: TestData = {
-        name: "Exemple TypeScript",
-        description: "Ceci est un test avec TSX",
-        createdAt: new Date(),
-      };
+  // const addTestData = async () => {
+  //   try {
+  //     const newDoc: TestData = {
+  //       name: "Exemple TypeScript",
+  //       description: "Ceci est un test avec TSX",
+  //       createdAt: new Date(),
+  //     };
 
-      const docRef = await addDoc(collection(db, "test"), newDoc);
-      console.log("Document ajouté avec ID :", docRef.id);
-    } catch (e) {
-      console.error("Erreur lors de l'ajout :", e);
-    }
-  };
-
+  //     const docRef = await addDoc(collection(db, "test"), newDoc);
+  //     console.log("Document ajouté avec ID :", docRef.id);
+  //   } catch (e) {
+  //     console.error("Erreur lors de l'ajout :", e);
+  //   }
+  // };
+  
   return (
-    <div>
-      <h2>Ajouter un document dans la collection "test"</h2>
-      <button onClick={addTestData}>Ajouter</button>
-    </div>
+    // <div>
+    //   <h2>Ajouter un document dans la collection "test"</h2>
+    //   <button onClick={addTestData}>Ajouter</button>
+    // </div>
+    <Form>
+      <FormGroup>
+        <Label for="exampleEmail" hidden>
+          Email
+        </Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Email"
+          type="email"
+        />
+      </FormGroup>{" "}
+      <FormGroup>
+        <Label for="examplePassword" hidden>
+          Password
+        </Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="Password"
+          type="password"
+        />
+      </FormGroup>{" "}
+      <Button>Submit</Button>
+    </Form>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root as HTMLElement).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
